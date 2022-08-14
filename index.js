ee3eba9a563bf32640f5919284fafa5fe252c95d
saikrishnadatt3842 <102149703+saikrishnadatt3842@users.noreply.github.com>
2022-08-14 15:27:28 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+console.log("sten");
+
+document.querySelector(".slidebar .toggle-btn").addEventListener("click",function(){
+    document.querySelector(".slidebar").classList.toggle("active");
+})
+
+console.log("sai");
